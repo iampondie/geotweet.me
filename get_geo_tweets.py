cc99a57575561e8512662fed6a961d613d63eb50
Jack Dwyer <jackjack.dwyer@gmail.com>
2013-06-25 19:16:36 +1000

    fixed timestamp, only getting en tweets

--- get_geo_tweets.py
+++ get_geo_tweets.py
@@ -8,25 +8,36 @@ from tweepy.streaming import StreamListener
 from mongokit import *
 import models
 import pymongo
+import settings
+from tweepy import TweepError
 
 class TwitterRecorder(StreamListener):
     def __init__(self):
         self.total = 0 #Read tweet db size
-        models.connection.geo_tweet_me.stream_tweets.create_index([('geo.coordinates', pymongo.GEO2D), ('created_at', 1)])
-
+        print pymongo.version
+        models.connection.geo_tweet_me.stream_tweets.create_index([
+            ('geo.coordinates', pymongo.GEO2D), 
+            ('created_at', 1),
+        ])
+        models.connection.geo_tweet_me.stream_tweets.create_index([
+        ('text','text'),
+        ])
 
     def to_datetime(self, value):
-        #return datetime object from created_at sting
+        #return datetime object from created_at string
         return datetime.datetime.fromtimestamp(int(time.mktime(parsedate(value))))  
-
 
     def handle_coordinates(self, data):
         json_data = json.loads(data)
-        if json_data.get('geo'):
+        #we only want tweets, that are geotagged and english
+        if json_data.get('geo') and json_data.get('lang') == 'en':
             needed_data = {} 
             for key, value in models.Tweet.structure.iteritems():
                 if key == "created_at":
                     needed_data[key] = self.to_datetime(json_data.get(key))
+                elif key == "geotweetme":
+                    needed_data[key] = {}
+                    needed_data[key]['active'] = False
                 elif isinstance(value, dict):
                     needed_data[key] = {}
                     for _key, _value in value.iteritems():
@@ -55,21 +66,23 @@ class TwitterRecorder(StreamListener):
     def on_error(self, status):
         print "ERROR: %s" % str(status)
 
+print settings.CONSUMER_KEY
+print settings.CONSUMER_SECRET
+print settings.ACCESS_TOKEN
+print settings.ACCESS_TOKEN_SECRET
 
 out = TwitterRecorder()
-consumer_key="JZsM9vWaCoxA0ERi6nszQ"
-consumer_secret="O2jQADMGmFHxxQQutEuzMXNlX1XJiVfVhV2pSGykl4"
-access_token="245598959-GvIQR0S6f33EtZLqrpndBXdQHE8goTsNUEf3swo"
-access_token_secret="qNOxGgtsx47L5FevMQvXrwlamTAQtqjh5vI8lud8S0"
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+
+try:
+    api = tweepy.API(auth)
+#   print api.me().name
+except TweepError as e:
+    print e
+    sys.exit()
 
 api = tweepy.API(auth)
-print api.me().name
-
-#temp to get index created
-
-
 stream = tweepy.Stream(auth, out)
 stream.sample()
 
